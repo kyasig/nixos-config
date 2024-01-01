df1774f5be60d03952367c3536f665a84546a970
kyasig <kevinyang177013@gmail.com>
2024-01-01 15:07:21 -0800

    playing with bash stuff

--- home.nix
+++ home.nix
@@ -20,11 +20,6 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -37,11 +32,13 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+  home.packages = with pkgs; [
+    hello
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
+  #home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
@@ -52,7 +49,7 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
+  #};
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. If you don't want to manage your shell through Home
--- shell/bash.nix
+++ shell/bash.nix
@@ -1,15 +1,22 @@
 { config, pkgs, ...}:
-
-{
- programs.bash = {
-    enable = true;
-    shellAliases = {
+let 
+  myAliases = {
       "xd" = "echo xd";
       "c" = "clear";
       "listgens" = "sudo nix-env -p /nix/var/nix/profiles/system --list-generations";
       "deletegens" = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations";
       "vim" = "nvim";
-    };
   };
+in 
+{
+ programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = myAliases;
+  };
+
+  home.packages = with pkgs;[
+    cowsay
+  ]
 }
 
