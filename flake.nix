c9fd3818b8386482c5dd2a0f45762f86ea1cbf6c
kyasig <92475855+kyasig@users.noreply.github.com>
2024-01-29 10:32:33 -0800

    fixed stupid formatting

--- flake.nix
+++ flake.nix
@@ -1,60 +1,58 @@
 {
-    description = "kyasig's nixos flake";
-
-	inputs = {
+  description = "kyasig's nixos flake";
+  inputs = {
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-23.11";
     
-		home-manager = {
-			url = "github:nix-community/home-manager/release-23.11";
-		  inputs.nixpkgs.follows = "nixpkgs";
-		};
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-     # inputs.nixkpgs.follows = "nixpkgs";
+      inputs.nixkpgs.follows = "nixpkgs";
     };
+
     nix-colors = {
       url = "github:misterio77/nix-colors";
     };
+
     stylix.url = "github:danth/stylix";
   };
-
-	outputs = {
+  outputs = inputs @{
     self,
     nixpkgs,
     stylix,
     home-manager, 
     ...
-  }@inputs:
-    let
-      lib = nixpkgs.lib;
-			system = "x86_64-linux";
-			pkgs = nixpkgs.legacyPackages.${system};
-      font = "mononoki";
-      fontpkg = pkgs.mononoki;
-      nvidia = true;
-      user = "ky";
-  	in
-		{
-     nixosConfigurations = {
-		   sig = lib.nixosSystem{
-         inherit system;
-		     modules = [./nixos/default.nix];
-       };
-		 };
-     homeConfigurations = {
-	   	 ky = home-manager.lib.homeManagerConfiguration{
-       	 inherit pkgs;
-		     modules = [
-          stylix.homeManagerModules.stylix
-          ./home-manager/default.nix
-         ];
-         extraSpecialArgs = {
-           inherit inputs;
-           inherit font;
-           inherit fontpkg;
-         };
-       };
-		 };
-	  };
+  }: let
+    system = "x86_64-linux";
+    inherit (nixkpkgs) lib
+    pkgs = nixpkgs.legacyPackages.${system};
+    font = "mononoki";
+    fontpkg = pkgs.mononoki;
+    nvidia = true;
+    user = "ky";
+  in{
+    nixosConfigurations = {
+      sig = lib.nixosSystem{
+        inherit system;
+        modules = [./nixos/default.nix];
+      };
+    };
+    homeConfigurations = {
+      ky = home-manager.lib.homeManagerConfiguration{
+        inherit pkgs;
+        modules = [
+        stylix.homeManagerModules.stylix
+        ./home-manager/default.nix
+        ];
+      extraSpecialArgs = {
+        inherit inputs;
+        inherit font;
+        inherit fontpkg;
+      };
+      };
+    };
+  };
 }
