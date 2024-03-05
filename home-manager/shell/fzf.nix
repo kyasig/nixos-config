f2e1359954e905279c6575b0c82f89c2d0cf4c61
kyasig <kevinyang177013@gmail.com>
2024-03-05 10:01:27 -0800

    more fzf configuration

--- home-manager/shell/fzf.nix
+++ home-manager/shell/fzf.nix
@@ -1,16 +1,25 @@
 { config, lib,  pkgs, ...}:
 {
-
  programs.fzf = {
   enable = true;
+  enableZshIntegration = true;
   defaultOptions = [
     "--border=double"
     "--color='dark,fg:magenta'"
     "--header='ESC to quit'"
     "--pointer='→'"
   ];
-  enableZshIntegration = true;
-  fileWidgetCommand = "nf";
+  historyWidgetOptions = [
+    "--border-label='╢ Shell History╟'"
+    "--prompt='Search Shell History: '"
+    "--height=100%"
+  ];
+  changeDirWidgetOptions = [
+    "--border-label='╢ CD ╟'"
+    "--prompt='Change Directory To: '"
+    "--preview ='eza -T --level=3 {}'"
+    "--height=100%"
+  ];
  };
 }
 
