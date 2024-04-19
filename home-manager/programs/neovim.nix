d892e4b692037d12740a159fc75bcdef90fa982d
kyasig <kevinyang177013@gmail.com>
2024-04-19 15:45:15 -0700

    neovim configuration changes

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -2,5 +2,55 @@
 {
   programs.neovim = {
     enable = true;
+    extraConfig = ''
+			set mouse=a
+			syntax on
+			set number
+    	set relativenumber
+			set termguicolors
+			set cursorline
+			set cursorcolumn
+
+			set backspace=indent,eol,start
+			set confirm
+      set autoindent
+
+			set expandtab
+			set shiftwidth=2
+			set tabstop=2
+
+			set ignorecase
+			set smartcase
+			set showmode
+			set hlsearch
+			set wildmenu
+
+			autocmd InsertEnter * norm zz
+			autocmd BufWritePre * %s/\s\+$//e
+
+      map <A-h> <C-w>h
+      map <A-j> <C-w>j
+      map <A-k> <C-w>k
+      map <A-l> <C-w>l
+
+      nnoremap S :%s//gI<Left><Left><Left>
+
+			:let mapleader = "\<Space>"
+      nnoremap <leader>nn :NERDTree<CR>
+      nnoremap <leader>nc :NERDTreeClose<CR>
+      nnoremap <leader>gg :Goyo<CR>
+
+    '';
+		extraLuaConfig = ''
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+		'';
+		plugins = with pkgs.vimPlugins;[
+			vim-airline
+			telescope-nvim
+      nerdtree
+      goyo-vim
+		];
   };
 }
