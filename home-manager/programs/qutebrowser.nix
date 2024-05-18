417a67d301286234238795778f0bd0db2dc5f486
kyasig <kevinyang177013@gmail.com>
2024-05-17 20:48:06 -0700

    updated qutebrowser and picom

--- home-manager/programs/qutebrowser.nix
+++ home-manager/programs/qutebrowser.nix
@@ -1,10 +1,69 @@
-{ pkgs, lib, config,inputs, nix-colors, ... }:
+{ pkgs, lib, config, ... }:
 {
-  programs.zathura = {
+  programs.qutebrowser = {
     enable = true;
-    colors = {
-      webpage.preferrred_color_scheme = "${config.colorScheme.palette.base00}"
-      tabs.bar.bg = "${config.colorScheme.palette.base00}"
-    }
-  };
-}
+    searchEngines = {
+     DEFAULT = "https://startpage.com/search?q={}";
+    };
+    settings = {
+      scrolling.smooth = false;
+      content.blocking.method = "both";
+      statusbar.show = "in-mode";
+      colors = {
+        statusbar = {
+          normal = {
+            bg = "#${config.colorScheme.palette.base00}";
+            fg = "#${config.colorScheme.palette.base07}";
+          };
+          command = {
+            bg = "#${config.colorScheme.palette.base00}";
+            fg = "#${config.colorScheme.palette.base07}";
+          };
+          insert = {
+            bg = "#${config.colorScheme.palette.base00}";
+            fg = "#${config.colorScheme.palette.base0B}";
+          };
+          url = {
+            success.http.fg = "#${config.colorScheme.palette.base0B}";
+            success.https.fg = "#${config.colorScheme.palette.base0B}";
+            fg = "#${config.colorScheme.palette.base07}";
+            error.fg = "#${config.colorScheme.palette.base08}";
+            warn.fg = "#${config.colorScheme.palette.base08}";
+            hover.fg = "#${config.colorScheme.palette.base0D}";
+          };
+        };
+        webpage = {
+          preferred_color_scheme = "${config.colorScheme.variant}";
+          darkmode.enabled = true;
+          darkmode.policy.images = "never";
+        };
+        tabs = {
+          indicator.stop = "#${config.colorScheme.palette.base0D}";
+          odd = {
+            fg = "#${config.colorScheme.palette.base07}";
+            bg = "#${config.colorScheme.palette.base00}";
+          };
+          even = {
+            fg = "#${config.colorScheme.palette.base07}";
+            bg = "#${config.colorScheme.palette.base01}";
+          };
+          selected = {
+            odd = {
+              fg = "#${config.colorScheme.palette.base00}";
+              bg = "#${config.colorScheme.palette.base0E}";
+            };
+            even = {
+              fg = "#${config.colorScheme.palette.base00}";
+              bg = "#${config.colorScheme.palette.base0E}";
+            };
+          };
+        };
+        hints = {
+          bg = "#${config.colorScheme.palette.base0E}";
+          fg = "#${config.colorScheme.palette.base00}";
+          match.fg = "#${config.colorScheme.palette.base0E}";
+        };
+      };
+    };
+   };
+ }
--- home-manager/wms/picom.nix
+++ home-manager/wms/picom.nix
@@ -1,41 +1,17 @@
 { config, lib, pkgs, ... }:
-let 
-  picomPackage = pkgs.picom;
-in{
-  services.picom = if picomPackage == pkgs.picom-allusive then{
+{
+services.picom = {
     enable = true;
-    package = picomPackage;
     vSync = true;
-    backend = "glx";
-    settings =  {
-      animation-open-exclude = [ # the animations are actually annoying lol
-         "class_g = 'xmobar'"
-       ];
-       animation-unmap-exclude = [
-         "class_g = 'xmobar'"
-       ];
-       blur-background = true; #BLURRING DOESNT WORK RAAAHHH
-       blur-method = "dual_kawase";
-       blur-strength = 5;
-
-       blur-whitelist = true;
-
-       blur-include = [
-        "class_g = 'Alacritty'"
-       ];
-    };
-  }else {
-    enable = true;
-    package = picomPackage;
-    vSync = true;
-    backend = "glx";
+    #backend = "glx";
     settings =  {
       blur ={
-        method = "dual_kawase"; 
-        size = 3.0;
-        strength = 6;
-        deviation = 5.0;
+        method = "dual_kawase";
+        #size = 3.0;
+        strength = 5;
+        #deviation = 5.0;
       };
     };
   };
 }
+
