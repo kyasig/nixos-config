e0dd0a26ab9e552c50b4988a72dae33020a32d3a
kyasig <kevinyang177013@gmail.com>
2024-05-21 15:55:21 -0700

    xmobar changes

--- home-manager/wms/xmobar.nix
+++ home-manager/wms/xmobar.nix
@@ -12,17 +12,18 @@
     , allDesktops = True
     , persistent = True
     , commands = [
-          Run Cpu ["-t", " <total>%"] 10
-        , Run Memory ["-t"," <used>m used"] 10
-        , Run Date " %a %b %d [%r]" "date" 10
-        , Run BatteryP ["ACAD", "BAT0", "hidpp_battery_0"] ["-t", " <left>%"] 3
-        , Run Uptime ["-t"," <hours>h <minutes>m"] 10
-        , Run DiskU [("/"," <free> free")] [] 3000
+          Run Cpu ["-t", "<total>%"] 10
+        , Run Memory ["-t","<used>m used"] 10
+        , Run Date "%a %b %d - %r" "date" 10
+        , Run BatteryP ["ACAD", "BAT0", "hidpp_battery_0"] ["-t", "<left>%"] 3
+        , Run Uptime ["-t","<days>d <hours>h <minutes>m"] 10
+        , Run DiskU [("/","<free> free")] [] 3000
         , Run UnsafeXMonadLog
         ]
     , sepChar = "%"
     , alignSep = "}{"
-    , template = "  %UnsafeXMonadLog% }{ <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0D}><fc=#${config.colorScheme.palette.base0D}>%memory%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0B}><fc=#${config.colorScheme.palette.base0B}>%cpu%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base08}><fc=#${config.colorScheme.palette.base08}>%uptime%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0A}><fc=#${config.colorScheme.palette.base0A}>%disku%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0C}><fc=#${config.colorScheme.palette.base0C}>%battery%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0E}><fc=#${config.colorScheme.palette.base0E}>%date%</fc></box> "
+    --, template = "  %UnsafeXMonadLog% }{ <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0D}><fc=#${config.colorScheme.palette.base0D}>%memory%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0B}><fc=#${config.colorScheme.palette.base0B}>%cpu%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base08}><fc=#${config.colorScheme.palette.base08}>%uptime%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0A}><fc=#${config.colorScheme.palette.base0A}>%disku%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0C}><fc=#${config.colorScheme.palette.base0C}>%battery%</fc></box> <box type=Bottom width=2 mb=2 color=#${config.colorScheme.palette.base0E}><fc=#${config.colorScheme.palette.base0E}>%date%</fc></box> "
+    , template = "  %UnsafeXMonadLog%  }{ <fc=#${config.colorScheme.palette.base0D}></fc>  %memory%   <fc=#${config.colorScheme.palette.base0B}></fc>  %cpu%   <fc=#${config.colorScheme.palette.base08}></fc>  %uptime%   <fc=#${config.colorScheme.palette.base0A}>󰒋</fc>  %disku%   <fc=#${config.colorScheme.palette.base0C}></fc>  %battery%   <fc=#${config.colorScheme.palette.base0E}>󰥔</fc>  %date%  "
 }
     '';
   };
