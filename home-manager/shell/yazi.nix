59ec8ecb3147b0fa3f1b3ce7420dda63a0c4f5fd
kyasig <kevinyang177013@gmail.com>
2024-07-05 22:46:53 -0700

    yazi configured through nix

--- home-manager/shell/yazi.nix
+++ home-manager/shell/yazi.nix
@@ -2,7 +2,15 @@
 {
  programs.yazi = {
    enable = true;
+   enableZshIntegration = true;
+   settings = {
+     manager = {
+       ratio = [1 5 5];
+       sort_by = "alphabetical";
+       sort_sensitive = false;
+       sort_reverse = false;
+       lineMode = "size";
+     };
+   };
  };
- home.file.".config/yazi/yazi.toml".source = ./yazi.toml;
 }
-
