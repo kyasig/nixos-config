5ff810a75fc76121902c72e7b46e56b51096764f
kyasig <kevinyang177013@gmail.com>
2024-09-23 13:24:56 -0700

    install and configure lualine and telescope

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -57,8 +57,41 @@
           texlab.enable = true;
         };
       };
+      lualine = {
+        enable = true;
+        alwaysDivideMiddle = true;
+        componentSeparators = {
+          left = "|";
+          right = "|";
+        };
+        sections = {
+          lualine_a = ["mode"];
+          lualine_b = [
+            "branch"
+            "diff"
+            "diagnostics"
+          ];
+          lualine_c = ["filename"];
+          lualine_x = ["filetype"];
+          lualine_y = ["progress"];
+          lualine_z = [''" " .. os.date("%R")''];
+        };
+      };
+      telescope = {
+        enable = true;
+        extensions = {
+          fzf-native.enable = true;
+        };
+        keymaps = {
+          "<leader>ff" = {
+            action = "find_files";
+          };
+          "<leader>fb" = {
+            action = "buffers";
+          };
+        };
+      };
     };
-
     colorschemes.base16 = {
       enable = true;
       colorscheme = {
