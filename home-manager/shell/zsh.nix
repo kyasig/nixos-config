a8cea6c010a793df92d90ebe612985f2a82919ec
kyasig <kevinyang177013@gmail.com>
2025-05-17 19:29:00 -0700

    fix deprecated thing

--- home-manager/shell/zsh.nix
+++ home-manager/shell/zsh.nix
@@ -15,26 +15,30 @@ let
     "mkdir" = "mkdir -vp";
     "n" = "nvim";
     "y" = "yazi";
-    "nf" = "fd -H -tf | fzf  --prompt='edit file: ' --preview 'bat --style=numbers --color=always {}' --border-label='╢Edit File╟'| xargs -r $EDITOR";
+    "nf" =
+      "fd -H -tf | fzf  --prompt='edit file: ' --preview 'bat --style=numbers --color=always {}' --border-label='╢Edit File╟'| xargs -r $EDITOR";
     "fpdf" = "fd -tf --glob '*.pdf' | fzf --border=double --prompt='Open PDF: ' | xargs  -r zathura";
-    "wall" = "fd -tf --full-path /home/ky/Pictures/Wallpapers | fzf --preview 'feh --bg-scale {}' --border-label='╢Change Wallpaper╟' --prompt='select wallpaper: '";
+    "wall" =
+      "fd -tf --full-path /home/ky/Pictures/Wallpapers | fzf --preview 'feh --bg-scale {}' --border-label='╢Change Wallpaper╟' --prompt='select wallpaper: '";
     "fy" = "yazi $(fd -t d | fzf)";
     "ac" = "ani-cli";
+    "nb" = "newsboat";
     "setwal" = "feh --bg-fil";
+    "z" = "zathura";
   };
 in
 {
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
+    autosuggestion.enable = true;
     shellAliases = myAliases;
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
     };
-    initExtra = ''
+    initContent = ''
       set -o vi
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
     '';
