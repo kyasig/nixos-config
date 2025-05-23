264321ea55c0add2a3eea19a5dfde34251223f83
kyasig <kevinyang177013@gmail.com>
2025-05-22 18:40:50 -0700

    bye bye channels

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,18 +1,25 @@
-{ ... }:
+{lib,inputs,...}:
 {
   nix = {
     settings = {
       experimental-features = [
         "nix-command"
         "flakes"
+        "pipe-operators"
       ];
       auto-optimise-store = true;
     };
     channel.enable = false;
+    registry = (lib.mapAttrs (_: flake: { inherit flake; }) inputs);
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
+    settings = {
+        nix-path = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
+        flake-registry = ""; # optional, ensures flakes are truly self-contained
+    };
     gc = {
       automatic = true;
       dates = "daily";
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 5d";
     };
   };
 }
