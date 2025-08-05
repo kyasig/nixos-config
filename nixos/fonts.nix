2ece693204d97d7a7b1c7ff5e057b1ba1ef8c3af
kyasig <kevinyang177013@gmail.com>
2025-08-04 23:36:32 -0700

    fix font stuff idk

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -2,14 +2,20 @@
 {
   fonts = {
     enableGhostscriptFonts = true;
+    fontDir.enable = true;
+    enableDefaultPackages = true;
     packages = with pkgs; [
-      noto-fonts
-      noto-fonts-extra
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
       inconsolata
       font-awesome
       mononoki
+      #nerd-fonts.jetbrains-mono
+      # (nerdfonts.override{
+      #   fonts = [
+      #    "JetBrainsMono"
+      #   ];
+      # })
     ];
   };
 }
